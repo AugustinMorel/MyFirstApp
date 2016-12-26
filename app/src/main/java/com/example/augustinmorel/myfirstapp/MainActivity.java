597ee4cd257dec6dc241e9ce98261f7bd15155c4
Augustin <morel.aug@gmail.com>
2016-12-26 12:46:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/augustinmorel/myfirstapp/MainActivity.java
@@ -0,0 +1,17 @@
+//N'oubliez pas de déclarer le bon package dans lequel se trouve le fichier !
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PremiereActivite extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Bonjour, vous me devez 1 000 000€.");
+        setContentView(text);
+    }
+}
